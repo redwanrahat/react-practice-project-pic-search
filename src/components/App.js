98fb577ac448c6f4792a099ea587969400c51c79
redwanrahat <rahat.lx@gmail.com>
2019-05-11 00:35:01 +0600

    we can now use the api of unsplash to search images

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,20 @@
 import React, {Component} from 'react';
-import Axios from 'axios'
+import unsplash from '../api/unsplash'
 import SearchBar from "./SearchBar"
 
 class App extends Component {
-    onSearchSubmit(term){
-        Axios.get('https://api.unsplash.com/search/photos', {
-            params: { query: term },
-            headers: {
-                Authorization: 'Client-ID 8ba51d2d2cf8188d6db33ac5cb6f6b07dca5fa70c0550964c6000709f76d464e'
-            }            
+    state = { images: [] }
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos', {
+            params: { query: term },          
         })
+        this.setState({images: response.data.results})
     }
     render() {
         return (
             <div className="ui container" style={{marginTop: "15px"}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                {this.state.images.length}
             </div>
         );
     }
